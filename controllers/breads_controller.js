57b90668f6f9150786e45f01f1c8de938f42dcea
tjs6595 <t.schultejans@schenckprocess.com>
2022-06-07 20:09:24 -0500

    Sending Index and Show Data.

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,14 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('index', 
+    {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
 })
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
